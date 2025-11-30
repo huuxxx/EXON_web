32f9460aedaecb33ff2b12af9d6bf23811b80e95
Huxley Dowling <hdowling87@hotmail.com>
2025-11-30 14:48:28 +1000

    remove env var

--- EXON_web/util/constants.ts
+++ EXON_web/util/constants.ts
@@ -5,7 +5,6 @@ export class Constants {
   static readonly STEAM_WEB_API_KEY = 'STEAM_WEB_API_KEY';
   static readonly APP_ID_DEMO = 'APP_ID_DEMO';
   static readonly APP_ID_FULL = 'APP_ID_FULL';
-  static readonly LEADERBOARD_NAME = 'LEADERBOARD_NAME';
   static readonly LEADERBOARD_TEST_ID = 'LEADERBOARD_TEST_ID';
   static readonly LEADERBOARD_DEMO_EASY_ID = 'LEADERBOARD_DEMO_EASY_ID';
   static readonly LEADERBOARD_DEMO_MEDIUM_ID = 'LEADERBOARD_DEMO_MEDIUM_ID';
